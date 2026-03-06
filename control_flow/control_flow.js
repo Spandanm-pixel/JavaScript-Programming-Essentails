54add14598c621a9c48e54ce13bf099e5e9b696d
Spandanm-pixel <spandanm197@gmail.com>
2026-03-06 05:13:33 -0500

    Control Flow

---
+++ control_flow/control_flow.js
@@ -0,0 +1,44 @@
+let userRole="admin";
+let accessLevel;
+
+if (userRole==="admin"){
+    accessLevel="Full Access Granted";
+} else if(userRole="Manager"){
+    accessLevel="Limited Access Granted";
+} else {
+    accessLevel="No Access Granted";
+}
+
+console.log("Access Level:",accessLevel)
+
+let isLoggedIn=true;
+let userMessage;
+
+if(isLoggedIn){
+    if(userRole==="Admin!"){
+        userMessage="Welcome, Admin!";
+    } else {
+        userMessage="Welcome, User!";
+    } 
+} else {
+    userMessage="Please Log in to access the system."
+}
+
+console.log("user Message:",userMessage)
+
+let userTypes = "subscriber"
+let userCategory;
+
+switch(userType){
+    case "admin":
+        userCategory ="Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+    case "subscriber":
+        userCategory="Subscriber";
+    default:
+        usercategory="unknown";
+}
+
+console.log("User Category:",userCategory);
